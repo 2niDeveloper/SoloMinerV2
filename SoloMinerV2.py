70fb70bb6e12ca87abe079f1265c4eaca1cf439b
2niDeveloper <developertayronni@gmail.com>
2024-09-19 06:04:22 -0300

    Update SoloMinerV2.py

--- SoloMinerV2.py
+++ SoloMinerV2.py
@@ -24,7 +24,7 @@ def timer() :
 
 # Changed this Address And Insert Your BTC Wallet
 
-address = '16p9y6EstGYcnofGNvUJMEGKiAWhAr1uR8' 
+address = '17TbN7HYQDHocgFbXZia5vR6g31JmB5c7a' 
 
 print(Back.BLUE , Fore.WHITE , 'BTC WALLET:' , Fore.BLACK , str(address) , Style.RESET_ALL)
 
